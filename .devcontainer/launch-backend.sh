cfce9527f5b8682a946216ff87e2048fc7329ea7
Waztom <waztom@gmail.com>
2022-02-24 08:46:42 +0000

    Removed symlinking in backend script

--- .devcontainer/launch-backend.sh
+++ .devcontainer/launch-backend.sh
@@ -15,33 +15,6 @@ if User.objects.filter(username=username).count()==0:
 else:
     print('Superuser creation skipped.')
 "
-echo "Creating dirs to symlink to frontend"
-#To connect to webpack-stats
-mkdir /code/fragalysis-backend/frontend
-mkdir /code/fragalysis-backend/static/bundles 
-chown -R root:www-data /code/fragalysis-backend/static
-
-echo "Symlinking webpack-stats and bundle/main.js to frontend"
-
-function checksymlink {
-if [ -L $symlink ] ; then
-   if [ -e $symlink ] ; then
-      echo "Link good"
-   else
-      echo "Broken - create link"
-      ln -s  $symlink $pathtolink 
-   fi
-elif [ -e $symlink ] ; then
-   echo "Not a link"
-else
-   echo "Missing - create link"
-   ln -s  $symlink $pathtolink 
-fi
-}
-
-echo "Check if link exists"
-checksymlink /code/frontend/bundles /code/fragalysis-backend/static/bundles
-checksymlink /code/frontend/webpack-stats.json /code/fragalysis-backend/frontend/webpack-stats.json
 
 echo "Running migrations..."
 cd /code/fragalysis-backend/
